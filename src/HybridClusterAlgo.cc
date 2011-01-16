83f53c79a60bdda363ce2722cbe0aea7a89621f4
cmsbuild <>
2011-01-16 08:39:56 +0000

    This commit was manufactured by cvs2git to create tag 'RecoEcal- EgammaClusterAlgos-argiro20110116'.
    Sprout from master 2010-10-26 23:39:31 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Convert BuildFiles to XML'
    Cherrypick from master 2011-01-16 08:39:54 UTC Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch> 'Modified EcalSeverityLevelAlgo interface':
        interface/HybridClusterAlgo.h
        src/HybridClusterAlgo.cc

--- src/HybridClusterAlgo.cc
+++ src/HybridClusterAlgo.cc
@@ -7,6 +7,7 @@
 #include <vector>
 #include <set>
 #include "RecoEcal/EgammaCoreTools/interface/ClusterEtLess.h"
+#include "RecoLocalCalo/EcalRecAlgos/interface/EcalSeverityLevelAlgo.h"
 
 //The real constructor
 HybridClusterAlgo::HybridClusterAlgo(double eb_str, 
@@ -35,7 +36,7 @@ HybridClusterAlgo::HybridClusterAlgo(double eb_str,
   
   
 {
-  spId_ = EcalSeverityLevelAlgo::SpikeId(severitySpikeId);
+
   dynamicPhiRoad_ = false;
   if ( debugLevel_ == pDEBUG ) {
     //std::cout << "dynamicEThres: " << dynamicEThres_ 
@@ -55,9 +56,9 @@ HybridClusterAlgo::HybridClusterAlgo(double eb_str,
 void HybridClusterAlgo::makeClusters(const EcalRecHitCollection*recColl, 
 				     const CaloSubdetectorGeometry*geometry,
 				     reco::BasicClusterCollection &basicClusters,
+				     const EcalSeverityLevelAlgo * sevLv,
 				     bool regional,
-				     const std::vector<EcalEtaPhiRegion>& regions,
-				     const EcalChannelStatus*chStatus
+				     const std::vector<EcalEtaPhiRegion>& regions
 				     )
 {
   //clear vector of seeds
@@ -125,12 +126,8 @@ void HybridClusterAlgo::makeClusters(const EcalRecHitCollection*recColl,
 	      excludedCrys_.insert(it->id());
 	    continue; // the recHit has to be excluded from seeding
 	  }
-	  int severityFlag =  EcalSeverityLevelAlgo::severityLevel( it->id(), 
-								  (*recHits_), 
-								  (*chStatus),
-								  severityRecHitThreshold_,
-								  spId_,
-								  severitySpikeThreshold_);
+
+	  int severityFlag =  sevLv->severityLevel( it->id(), *recHits_);
 	  std::vector<int>::const_iterator sit = std::find( v_severitylevel_.begin(), v_severitylevel_.end(), severityFlag);
 	  if (debugLevel_ == pDEBUG){
 	    std::cout << "found flag: " << severityFlag << std::endl;
